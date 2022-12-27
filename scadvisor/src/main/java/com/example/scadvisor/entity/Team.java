7281c9470e08e51b664c0c9f7dd1f8283cb08e56
Carlos Luciano Jimenez Lopez <cakillos1998@gmail.com>
2022-12-27 21:29:53 +0100

    create player, team and match

---
+++ scadvisor/src/main/java/com/example/scadvisor/entity/Team.java
@@ -0,0 +1,19 @@
+package com.example.scadvisor.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Equipo {
+
+    
+}
